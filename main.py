17226bed292c07383d90b8ff4fefd6c24e611eee
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2017-10-09 19:23:40 -0300

    Refactoring

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-"""NApp responsible for installing or removing flows on the switches."""
-
+"""kytos/flow_manager NApp installs, lists and deletes switch flows."""
 import json
 
 from flask import request
-
 from kytos.core import KytosEvent, KytosNApp, log, rest
 
-from pyof.foundation.network_types import HWAddress
-
-from pyof.v0x01.common.action import (ActionOutput as ActionOutput10,
-                                      ActionType as ActionType10,
-                                      ActionVlanVid)
-from pyof.v0x01.controller2switch.flow_mod import (FlowMod as FlowMod10,
-                                                   FlowModCommand)
-
-from pyof.v0x04.common.action import (ActionOutput as ActionOutput13,
-                                      ActionSetField, ActionType as
-                                      ActionType13)
-from pyof.v0x04.common.flow_instructions import (InstructionApplyAction,
-                                                 InstructionType as IType)
-from pyof.v0x04.common.flow_match import OxmOfbMatchField, OxmTLV, VlanId
-from pyof.v0x04.controller2switch.flow_mod import FlowMod as FlowMod13
-
-from napps.kytos.of_flow_manager import settings
+from napps.kytos.of_flow_manager.serializers.base import FlowSerializer
+from napps.kytos.of_flow_manager.serializers.v0x01 import FlowSerializer10
+from napps.kytos.of_flow_manager.serializers.v0x04 import FlowSerializer13
 
 
 class Main(KytosNApp):
-    """Main class of of_stats NApp."""
+    """Main class to be used by Kytos controller."""
 
     def setup(self):
         """Replace the 'init' method for the KytosApp subclass.
@@ -34,11 +18,12 @@ class Main(KytosNApp):
         The setup method is automatically called by the run method.
         Users shouldn't call this method directly.
         """
-        self.parser = FlowParser()
         log.debug("flow-manager starting")
+        self._serializer10 = FlowSerializer10()
+        self._serializer13 = FlowSerializer13()
 
     def execute(self):
-        """Method to be runned once on app 'start' or in a loop.
+        """Run once on NApp 'start' or in a loop.
 
         The execute method is called by the run method of KytosNApp class.
         Users shouldn't call this method directly.
@@ -51,230 +36,67 @@ class Main(KytosNApp):
 
     @rest('flows')
     @rest('flows/<dpid>')
-    def retrieve_flows(self, dpid=None):
+    def list(self, dpid=None):
         """Retrieve all flows from a switch identified by dpid.
 
-        If no dpid has been specified, returns the flows from all switches.
+        If no dpid is specified, return all flows from all switches.
         """
+        dpids = [dpid] if dpid else self.controller.switches
+        switches = [self.controller.get_switch_by_dpid(dpid) for dpid in dpids]
+
         switch_flows = {}
 
-        if dpid:
-            target = [dpid]
-        else:
-            target = self.controller.switches
-
-        for switch_dpid in target:
-            switch = self.controller.get_switch_by_dpid(switch_dpid)
-            flows = []
-            for flow in switch.flows:
-                if switch.connection.protocol.version == 0x04:
-                    flow_dict = self.parser.flow13_as_dict(flow)
-                else:
-                    flow_dict = self.parser.flow10_as_dict(flow)
-                flows.append(flow_dict)
-            switch_flows[switch_dpid] = flows
+        for switch in switches:
+            serializer = self._get_serializer(switch)
+            flows_dict = [serializer.to_dict(flow) for flow in switch.flows]
+            switch_flows[switch.dpid] = flows_dict
         return json.dumps(switch_flows)
 
-    @rest('add-flows', methods=['POST'])
-    @rest('add-flows/<dpid>', methods=['POST'])
-    def insert_flows(self, dpid=None):
+    @rest('flows', methods=['POST'])
+    @rest('flows/<dpid>', methods=['POST'])
+    def add(self, dpid=None):
         """Install new flows in the switch identified by dpid.
 
-        If no dpid has been specified, install flows in all switches.
+        If no dpid is specified, install flows in all switches.
         """
-        if dpid:
-            target = [dpid]
-        else:
-            target = self.controller.switches
-
-        json_content = request.get_json()
-        for switch_dpid in target:
-            switch = self.controller.get_switch_by_dpid(switch_dpid)
-            for flow in json_content:
-                if switch.connection.protocol.version == 0x04:
-                    flow_mod = self.parser.flowmod13_from_dict(flow)
-                else:
-                    flow_mod = self.parser.flowmod10_from_dict(flow)
-
-                flow_mod.command = FlowModCommand.OFPFC_ADD
-
-                event = KytosEvent(name=('kytos/of_flow_manager.messages.out.'
-                                   'ofpt_flow_mod'),
-                                   content={'destination': switch.connection,
-                                            'message': flow_mod})
-
-                self.controller.buffers.msg_out.put(event)
-
+        self._send_events(FlowSerializer.OFPFC_ADD, dpid)
         return json.dumps({"response": "FlowMod Messages Sent"}), 201
 
-    @rest('del-flows', methods=['POST'])
-    @rest('del-flows/<dpid>', methods=['POST'])
-    def delete_flows(self, dpid=None):
+    @rest('delete', methods=['POST'])
+    @rest('delete/<dpid>', methods=['POST'])
+    def delete(self, dpid=None):
         """Delete existing flows in the switch identified by dpid.
 
-        If no dpid has been specified, delete flows from all switches.
+        If no dpid is specified, delete flows from all switches.
         """
-        if dpid:
-            target = [dpid]
-        else:
-            target = self.controller.switches
-
-        json_content = request.get_json()
-        for switch_dpid in target:
-            switch = self.controller.get_switch_by_dpid(switch_dpid)
-            for flow in json_content:
-                if switch.connection.protocol.version == 0x04:
-                    flow_mod = self.parser.flowmod13_from_dict(flow)
-                else:
-                    flow_mod = self.parser.flowmod10_from_dict(flow)
-
-                flow_mod.command = FlowModCommand.OFPFC_DELETE
-
-                event = KytosEvent(name=('kytos/of_flow_manager.messages.out.'
-                                   'ofpt_flow_mod'),
-                                   content={'destination': switch.connection,
-                                            'message': flow_mod})
-
-                self.controller.buffers.msg_out.put(event)
-
+        self._send_events(FlowSerializer.OFPFC_DELETE, dpid)
         return json.dumps({"response": "FlowMod Messages Sent"}), 202
 
+    def _send_events(self, command, dpid=None):
+        """Create FlowMods from HTTP request and send to switches."""
+        event_name = 'kytos/of_flow_manager.messages.out.ofpt_flow_mod'
+        if dpid:
+            switches = [self.controller.get_switch_by_dpid(dpid)]
+        else:
+            switches = self.controller.switches
 
-class FlowParser(object):
-    """Class responsible for manipulating flows at the switches."""
+        for switch in switches:
+            connection = switch.connection
+            serializer = self._get_serializer(switch)
+            for flow_dict in request.get_json():
+                event = KytosEvent(event_name, {'destination': connection})
+                self._send_event(flow_dict, serializer, command, event)
 
-    flow_attributes = ['table_id', 'priority', 'idle_timeout', 'hard_timeout',
-                       'cookie']
-    match_attributes = {'in_port': OxmOfbMatchField.OFPXMT_OFB_IN_PORT,
-                        'dl_src': OxmOfbMatchField.OFPXMT_OFB_ETH_SRC,
-                        'dl_dst': OxmOfbMatchField.OFPXMT_OFB_ETH_DST,
-                        'dl_type': OxmOfbMatchField.OFPXMT_OFB_ETH_TYPE,
-                        'dl_vlan': OxmOfbMatchField.OFPXMT_OFB_VLAN_VID,
-                        'dl_vlan_pcp': OxmOfbMatchField.OFPXMT_OFB_VLAN_PCP}
-    match_13to10 = {b: a for a, b in match_attributes.items()}
+    def _send_event(self, flow_dict, serializer, command, event):
+        """Create and send one FlowMod to one switch."""
+        # Create FlowMod message
+        flow_mod = serializer.from_dict(flow_dict)
+        flow_mod.command = command
+        # Complete and send KytosEvent
+        event.content['message'] = flow_mod
+        self.controller.buffers.msg_out.put(event)
 
-    def flowmod10_from_dict(self, dictionary):
-        """Return an OF1.0 FlowMod message created from input dictionary."""
-
-        flow_mod = FlowMod10()
-
-        for field, data in dictionary.items():
-            if field in self.flow_attributes:
-                setattr(flow_mod, field, data)
-            elif field == 'match':
-                for match_field, match_data in data.items():
-                    if match_field in self.match_attributes:
-                        setattr(flow_mod.match, match_field, match_data)
-            elif field == 'actions':
-                for action_type, action_data in data.items():
-                    if action_type == 'set_vlan':
-                        action = ActionVlanVid(vlan_id=action_data)
-                        flow_mod.actions.append(action)
-                    elif action_type == 'output':
-                        action = ActionOutput10(port=action_data)
-                        flow_mod.actions.append(action)
-
-        return flow_mod
-
-    def flow10_as_dict(self, flowstats):
-        """Return a dictionary created from input 1.0 switch's flows."""
-
-        flow_dict = {}
-        for field, data in vars(flowstats).items():
-            if field in self.flow_attributes:
-                flow_dict[field] = data.value
-
-        flow_dict['match'] = {}
-        for field, data in vars(flowstats.match).items():
-            if field in self.match_attributes:
-                flow_dict['match'][field] = data.value
-
-        flow_dict['actions'] = {}
-        for action in flowstats.actions:
-            if action.action_type == ActionType10.OFPAT_SET_VLAN_VID:
-                flow_dict['actions']['set_vlan'] = action.vlan_id.value
-            elif action.action_type == ActionType10.OFPAT_OUTPUT:
-                flow_dict['actions']['output'] = action.port.value
-
-        return flow_dict
-
-    def flowmod13_from_dict(self, dictionary):
-        """Return an OF1.3 FlowMod message created from input dictionary."""
-
-        flow_mod = FlowMod13()
-        instruction = InstructionApplyAction()
-        flow_mod.instructions.append(instruction)
-
-        for field, data in dictionary.items():
-            if field in self.flow_attributes:
-                setattr(flow_mod, field, data)
-            elif field == 'match':
-                for match_field, match_data in data.items():
-                    if match_field in self.match_attributes:
-                        tlv = OxmTLV()
-                        tlv.oxm_field = self.match_attributes[match_field]
-                        if match_field == 'dl_vlan_pcp':
-                            tlv.oxm_value = match_data.to_bytes(1, 'big')
-                        elif match_field == 'dl_vlan':
-                            vid = match_data | VlanId.OFPVID_PRESENT
-                            tlv.oxm_value = vid.to_bytes(2, 'big')
-                        elif match_field in ('dl_src', 'dl_dst'):
-                            addr = HWAddress(match_data)
-                            tlv.oxm_value = addr.pack()
-                        elif match_field == 'in_port':
-                            tlv.oxm_value = match_data.to_bytes(4, 'big')
-                        else:
-                            tlv.oxm_value = match_data.to_bytes(2, 'big')
-
-                        flow_mod.match.oxm_match_fields.append(tlv)
-
-            elif field == 'actions':
-                for action_type, action_data in data.items():
-                    if action_type == 'set_vlan':
-                        tlv = OxmTLV()
-                        tlv.oxm_field = OxmOfbMatchField.OFPXMT_OFB_VLAN_VID
-                        vid = action_data | VlanId.OFPVID_PRESENT
-                        tlv.oxm_value = vid.to_bytes(2, 'big')
-                        action = ActionSetField(field=tlv)
-                        instruction.actions.append(action)
-                    elif action_type == 'output':
-                        action = ActionOutput13(port=action_data)
-                        instruction.actions.append(action)
-
-        return flow_mod
-
-    def flow13_as_dict(self, flowstats):
-        """Return a dictionary created from input 1.3 switch's flows."""
-
-        flow_dict = {}
-        for field, data in vars(flowstats).items():
-            if field in self.flow_attributes:
-                flow_dict[field] = data.value
-
-        flow_dict['match'] = {}
-        for field in flowstats.match.oxm_match_fields:
-            if field.oxm_field in self.match_13to10:
-                match_field = self.match_13to10.get(field.oxm_field)
-                if match_field == 'dl_vlan':
-                    data = int.from_bytes(field.oxm_value, 'big') & 4095
-                elif match_field in ('dl_src', 'dl_dst'):
-                    addr = HWAddress()
-                    addr.unpack(field.oxm_value)
-                    data = str(addr)
-                else:
-                    data = int.from_bytes(field.oxm_value, 'big')
-                flow_dict['match'][match_field] = data
-
-        flow_dict['actions'] = {}
-        for instruction in flowstats.instructions:
-            if instruction.instruction_type == IType.OFPIT_APPLY_ACTIONS:
-                for action in instruction.actions:
-                    if action.action_type == ActionType13.OFPAT_SET_FIELD:
-                        if action.field.oxm_field == OxmOfbMatchField.OFPXMT_OFB_VLAN_VID:
-                            data = int.from_bytes(action.field.oxm_value,
-                                                  'big') & 4095
-                        flow_dict['actions']['set_vlan'] = data
-                    elif action.action_type == ActionType13.OFPAT_OUTPUT:
-                        flow_dict['actions']['output'] = action.port.value
-
-        return flow_dict
+    def _get_serializer(self, switch):
+        """Return the serializer with for the switch OF protocol version."""
+        version = switch.connection.protocol.version
+        return self._serializer10 if version == 0x01 else self._serializer13
---
+++ serializers/v0x04.py
@@ -0,0 +1,141 @@
+"""Flow serializer for OF 1.3."""
+from itertools import chain
+
+from pyof.foundation.network_types import HWAddress
+from pyof.v0x04.common.action import ActionOutput, ActionSetField, ActionType
+from pyof.v0x04.common.flow_instructions import InstructionType as IType
+from pyof.v0x04.common.flow_instructions import InstructionApplyAction
+from pyof.v0x04.common.flow_match import OxmOfbMatchField, OxmTLV, VlanId
+from pyof.v0x04.controller2switch.flow_mod import FlowMod
+
+from napps.kytos.of_flow_manager.serializers.base import FlowSerializer
+
+
+class FlowSerializer13(FlowSerializer):
+    """Flow serializer for OpenFlow 1.3."""
+
+    def __init__(self):
+        """Initialize OF 1.3 specific variables."""
+        super().__init__()
+        self._match_names = {
+            'in_port': OxmOfbMatchField.OFPXMT_OFB_IN_PORT,
+            'dl_src': OxmOfbMatchField.OFPXMT_OFB_ETH_SRC,
+            'dl_dst': OxmOfbMatchField.OFPXMT_OFB_ETH_DST,
+            'dl_type': OxmOfbMatchField.OFPXMT_OFB_ETH_TYPE,
+            'dl_vlan': OxmOfbMatchField.OFPXMT_OFB_VLAN_VID,
+            'dl_vlan_pcp': OxmOfbMatchField.OFPXMT_OFB_VLAN_PCP}
+        # Invert match_values index
+        self._match_values = {b: a for a, b in self._match_names.items()}
+
+    def from_dict(self, dictionary):
+        """Return an OF 1.0 FlowMod message from serialized dictionary."""
+        flow_mod = FlowMod()
+        instruction = InstructionApplyAction()
+        flow_mod.instructions.append(instruction)
+
+        for field, data in dictionary.items():
+            if field in self.flow_attributes:
+                setattr(flow_mod, field, data)
+            elif field == 'match':
+                tlvs = self._match_from_dict(data)
+                flow_mod.match.oxm_match_fields.append(list(tlvs))
+            elif field == 'actions':
+                actions = self._actions_from_dict(data)
+                instruction.actions.extend(list(actions))
+
+        return flow_mod
+
+    def _match_from_dict(self, dictionary):
+        known_fields = ((field, data) for field, data in dictionary.items()
+                        if field in self._match_names)
+        for field_name, data in known_fields:
+            tlv = OxmTLV()
+            tlv.oxm_field = self._match_names[field_name]
+            # set oxm_value
+            if field_name == 'dl_vlan_pcp':
+                tlv.oxm_value = data.to_bytes(1, 'big')
+            elif field_name == 'dl_vlan':
+                vid = data | VlanId.OFPVID_PRESENT
+                tlv.oxm_value = vid.to_bytes(2, 'big')
+            elif field_name in ('dl_src', 'dl_dst'):
+                tlv.oxm_value = HWAddress(data).pack()
+            elif field_name == 'in_port':
+                tlv.oxm_value = data.to_bytes(4, 'big')
+            else:
+                tlv.oxm_value = data.to_bytes(2, 'big')
+            yield tlv
+
+    @classmethod
+    def _actions_from_dict(cls, dictionary):
+        for action_type, data in dictionary.items():
+            action = cls._action_from_dict(action_type, data)
+            if action:
+                yield action
+
+    @classmethod
+    def _action_from_dict(cls, action_type, data):
+        if action_type == 'set_vlan':
+            tlv = cls._create_vlan_tlv(vlan_id=data)
+            return ActionSetField(field=tlv)
+        elif action_type == 'output':
+            return ActionOutput(port=data)
+
+    @staticmethod
+    def _create_vlan_tlv(vlan_id):
+        tlv = OxmTLV()
+        tlv.oxm_field = OxmOfbMatchField.OFPXMT_OFB_VLAN_VID
+        oxm_value = vlan_id | VlanId.OFPVID_PRESENT
+        tlv.oxm_value = oxm_value.to_bytes(2, 'big')
+        return tlv
+
+    def to_dict(self, flow_stats):
+        """Return a dictionary created from input 1.3 switch's flows."""
+        flow_dict = {field: data.value
+                     for field, data in vars(flow_stats).items()
+                     if field in self.flow_attributes}
+        flow_dict['match'] = self._match_to_dict(flow_stats)
+        flow_dict['actions'] = self._actions_to_dict(flow_stats)
+        return flow_dict
+
+    def _match_to_dict(self, flow_stats):
+        match_dict = {}
+        fields = (field for field in flow_stats.match.oxm_match_fields
+                  if field.oxm_field in self._match_values)
+        for field in fields:
+            match_field = self._match_values[field.oxm_field]
+            if match_field == 'dl_vlan':
+                data = int.from_bytes(field.oxm_value, 'big') & 4095
+            elif match_field in ('dl_src', 'dl_dst'):
+                addr = HWAddress()
+                addr.unpack(field.oxm_value)
+                data = str(addr)
+            else:
+                data = int.from_bytes(field.oxm_value, 'big')
+            match_dict[match_field] = data
+        return match_dict
+
+    def _actions_to_dict(self, flow_stats):
+        actions_dict = {}
+        for action in self._filter_actions(flow_stats):
+            action_dict = self._action_to_dict(action)
+            actions_dict.update(action_dict)
+        return actions_dict
+
+    @staticmethod
+    def _action_to_dict(action):
+        if action.action_type == ActionType.OFPAT_SET_FIELD:
+            if action.field.oxm_field == OxmOfbMatchField.OFPXMT_OFB_VLAN_VID:
+                data = int.from_bytes(action.field.oxm_value, 'big') & 4095
+            return {'set_vlan': data}
+        elif action.action_type == ActionType.OFPAT_OUTPUT:
+            return {'output': action.port.value}
+        return {}
+
+    @staticmethod
+    def _filter_actions(flow_stats):
+        """Filter instructions and return a list of their actions."""
+        instructions = (inst for inst in flow_stats.instructions
+                        if inst.instruction_type == IType.OFPIT_APPLY_ACTIONS)
+        action_lists = (instruction.actions for instruction in instructions)
+        # Make a list from a list of lists
+        return chain.from_iterable(action_lists)
