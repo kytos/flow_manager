c8a0eecd5491e6faacb6274faf34761386c43b7c
Jose Mauro <z.mauro@live.com>
2019-03-13 22:18:39 -0300

    3 pylint issues added to setup.cfg to be ignored to make possible pass through scrutinizer check

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ class Linter(SimpleCommand):
     def run(self):
         """Run yala."""
         print('Yala is running. It may take several seconds...')
-        check_call('yala setup.py', shell=True)
+        check_call('yala *.py', shell=True)
 
 
 class CITest(SimpleCommand):
