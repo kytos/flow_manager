05dde906be6ca7d2613a38c1962166f57bf89469
Humberto Diógenes <hdiogenes@gmail.com>
2020-07-07 20:06:51 -0300

    Version bump to 2.3 (#84)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if 'bdist_wheel' in sys.argv:
 BASE_ENV = Path(os.environ.get('VIRTUAL_ENV', '/'))
 
 NAPP_NAME = 'flow_manager'
-NAPP_VERSION = '2.2.2'
+NAPP_VERSION = '2.3'
 
 # Kytos var folder
 VAR_PATH = BASE_ENV / 'var' / 'lib' / 'kytos'
