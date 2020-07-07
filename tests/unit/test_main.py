b493dbdaecd1a69b6c40e3ecd03d33f5b6e1b07a
Gleyberson Andrade <gleybersonandrade@gmail.com>
2020-07-07 14:34:43 -0300

    Fix handle_errors method test
    Tests were failed because the assert was made without error_type and error_code. This commit solves this problem.

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -172,8 +172,12 @@ class TestMain(TestCase):
 
         message = MagicMock()
         message.header.xid.value = 0
+        message.error_type = 'type'
+        message.code = 123
         event = get_kytos_event_mock(name='.*.of_core.*.ofpt_error',
                                      content={'message': message})
         self.napp.handle_errors(event)
 
-        mock_send_napp_event.assert_called_with(flow.switch, flow, 'error')
+        mock_send_napp_event.assert_called_with(flow.switch, flow, 'error',
+                                                error_type='type',
+                                                error_code=123)
