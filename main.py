8cf062fca96f9ebaa9b26cd80b5bd36a904d3628
Carlos Magno <cmagnobarbosa@gmail.com>
2020-10-06 11:57:48 -0300

    Add flow persistence
    Now, all flows installed by 'kytos/flow_manager' are stored using 'kytos/storehouse'.
    
    Co-authored-by: José Mauro <z.mauro@live.com>
    Co-authored-by: Humberto Diógenes <hdiogenes@gmail.com>

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import jsonify, request
 
 from kytos.core import KytosEvent, KytosNApp, log, rest
 from kytos.core.helpers import listen_to
+from napps.kytos.flow_manager.storehouse import StoreHouse
 from napps.kytos.of_core.flow import FlowFactory
 
 from .exceptions import InvalidCommandError
@@ -24,16 +25,113 @@ class Main(KytosNApp):
         self._flow_mods_sent = OrderedDict()
         self._flow_mods_sent_max_size = FLOWS_DICT_MAX_SIZE
 
+        # Storehouse client to save and restore flow data:
+        self.storehouse = StoreHouse(self.controller)
+
+        # Format of stored flow data:
+        # {'flow_persistence': {'dpid_str': {'flow_list': [
+        #                                     {'command': '<add|delete>',
+        #                                      'flow': {flow_dict}}]}}}
+        self.stored_flows = {}
+        self.resent_flows = set()
+
     def execute(self):
         """Run once on NApp 'start' or in a loop.
 
         The execute method is called by the run method of KytosNApp class.
         Users shouldn't call this method directly.
         """
+        self._load_flows()
 
     def shutdown(self):
         """Shutdown routine of the NApp."""
         log.debug("flow-manager stopping")
+
+    @listen_to('kytos/topology.port.created')
+    def resend_stored_flows(self, event):
+        """Resend stored Flows."""
+        dpid = str(event.content['switch'])
+        switch = self.controller.get_switch_by_dpid(dpid)
+        # This can be a problem because this code is running a thread
+        if dpid in self.resent_flows:
+            log.info(f'Flow already resended to Switch {dpid}')
+            return
+        if dpid in self.stored_flows:
+            flow_list = self.stored_flows[dpid]['flow_list']
+            for flow in flow_list:
+                command = flow['command']
+                flows_dict = {"flows": [flow['flow']]}
+                self._install_flows(command, flows_dict, [switch])
+            self.resent_flows.add(dpid)
+            log.info(f'Flows resent to Switch {dpid}')
+
+    # pylint: disable=attribute-defined-outside-init
+    def _load_flows(self):
+        """Load stored flows."""
+        try:
+            data = self.storehouse.get_data()['flow_persistence']
+            if 'id' in data:
+                del data['id']
+            self.stored_flows = data
+
+        except KeyError as error:
+            log.debug(f'There are no flows to load: {error}')
+        else:
+            log.info('Flows loaded.')
+
+    def _store_changed_flows(self, command, flow, switch):
+        """Store changed flows.
+
+        Args:
+            command: Flow command to be installed
+            flow: Flows to be stored
+            switch: Switch target
+        """
+        stored_flows_box = self.stored_flows.copy()
+        # if the flow has a destination dpid it can be stored.
+        if not switch:
+            log.info('The Flow cannot be stored, the destination switch '
+                     f'have not been specified: {switch}')
+            return
+
+        installed_flow = {}
+        flow_list = []
+        installed_flow['command'] = command
+        installed_flow['flow'] = flow
+
+        serializer = FlowFactory.get_class(switch)
+        installed_flow_obj = serializer.from_dict(flow, switch)
+
+        if switch.id not in stored_flows_box:
+            # Switch not stored, add to box.
+            flow_list.append(installed_flow)
+            stored_flows_box[switch.id] = {'flow_list': flow_list}
+        else:
+            stored_flows = stored_flows_box[switch.id].get('flow_list', [])
+            # Check if flow already stored
+            for stored_flow in stored_flows:
+                stored_flow_obj = serializer.from_dict(stored_flow['flow'],
+                                                       switch)
+                if installed_flow_obj == stored_flow_obj:
+                    if stored_flow['command'] == installed_flow['command']:
+                        log.debug('Data already stored.')
+                        return
+                    # Flow with inconsistency in "command" fields : Remove the
+                    # old instruction. This happens when there is a stored
+                    # instruction to install the flow, but the new instruction
+                    # is to remove it. In this case, the old instruction is
+                    # removed and the new one is stored.
+                    stored_flow['command'] = installed_flow.get('command')
+                    stored_flows.remove(stored_flow)
+                    break
+
+            stored_flows.append(installed_flow)
+            stored_flows_box[switch.id]['flow_list'] = stored_flows
+
+        stored_flows_box['id'] = 'flow_persistence'
+        self.storehouse.save_flow(stored_flows_box)
+        del stored_flows_box['id']
+        self.stored_flows = stored_flows_box.copy()
 
     @rest('v2/flows')
     @rest('v2/flows/<dpid>')
@@ -80,12 +178,12 @@ class Main(KytosNApp):
         switches = self.controller.switches.values()
         return [switch for switch in switches if switch.is_enabled()]
 
-    def _send_flow_mods_from_request(self, dpid, command):
+    def _send_flow_mods_from_request(self, dpid, command, flows_dict=None):
         """Install FlowsMods from request."""
-        flows_dict = request.get_json()
-
         if flows_dict is None:
-            return jsonify({"response": 'flows dict is none.'}), 404
+            flows_dict = request.get_json()
+            if flows_dict is None:
+                return jsonify({"response": 'flows dict is none.'}), 404
 
         if dpid:
             switch = self.controller.get_switch_by_dpid(dpid)
@@ -124,6 +222,7 @@ class Main(KytosNApp):
                 self._add_flow_mod_sent(flow_mod.header.xid, flow, command)
 
                 self._send_napp_event(switch, flow, command)
+                self._store_changed_flows(command, flow_dict, switch)
 
     def _add_flow_mod_sent(self, xid, flow, command):
         """Add the flow mod to the list of flow mods sent."""
