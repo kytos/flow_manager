faf954efbcbfe013bb8ca898f54827d258a3ff06
josemauro <z.mauro@live.com>
2021-05-27 10:56:17 -0300

    Version bump to 4.0 (#121)

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if 'bdist_wheel' in sys.argv:
 BASE_ENV = Path(os.environ.get('VIRTUAL_ENV', '/'))
 
 NAPP_NAME = 'flow_manager'
-NAPP_VERSION = '3.0'
+NAPP_VERSION = '4.0'
 
 # Kytos var folder
 VAR_PATH = BASE_ENV / 'var' / 'lib' / 'kytos'
